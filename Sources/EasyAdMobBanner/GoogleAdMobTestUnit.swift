3682f23095f9c00525077fc559f027a476a404f3
Chen-Hai Teng <chenhaiteng@gmail.com>
2024-03-06 20:55:19 +0800

    publishing test unit

--- Sources/EasyAdMobBanner/GoogleAdMobTestUnit.swift
+++ Sources/EasyAdMobBanner/GoogleAdMobTestUnit.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 public enum GoogleAdMobTestUnit {
-    static let banner = "ca-app-pub-3940256099942544/2934735716"
-    static let adaptiveBanner = "ca-app-pub-3940256099942544/2435281174"
-    static let appOpen = "ca-app-pub-3940256099942544/5662855259"
-    static let interstitial = "ca-app-pub-3940256099942544/4411468910"
-    static let interstitialVideo = "ca-app-pub-3940256099942544/5135589807"
-    static let rewarded = "ca-app-pub-3940256099942544/1712485313"
-    static let rewardedInterstitial = "ca-app-pub-3940256099942544/6978759866"
-    static let nativeAdvanced = "ca-app-pub-3940256099942544/3986624511"
-    static let nativeAdvancedVidoe = "ca-app-pub-3940256099942544/2521693316"
+    public static let banner = "ca-app-pub-3940256099942544/2934735716"
+    public static let adaptiveBanner = "ca-app-pub-3940256099942544/2435281174"
+    public static let appOpen = "ca-app-pub-3940256099942544/5662855259"
+    public static let interstitial = "ca-app-pub-3940256099942544/4411468910"
+    public static let interstitialVideo = "ca-app-pub-3940256099942544/5135589807"
+    public static let rewarded = "ca-app-pub-3940256099942544/1712485313"
+    public static let rewardedInterstitial = "ca-app-pub-3940256099942544/6978759866"
+    public static let nativeAdvanced = "ca-app-pub-3940256099942544/3986624511"
+    public static let nativeAdvancedVidoe = "ca-app-pub-3940256099942544/2521693316"
 }
