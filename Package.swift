03f6a05cd559ad56a248c8e3f5fe745d739b92e5
Chen-Hai Teng <chenhaiteng@gmail.com>
2024-03-07 16:59:53 +0800

    Update package support version to backward compatible

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "EasyAdMobBanner",
-    platforms: [.macOS(.v14), .iOS(.v14), .tvOS(.v13)],
+    platforms: [.macOS(.v11), .iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
